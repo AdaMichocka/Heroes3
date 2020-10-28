fa2baad8f335f8a5c96a34ee36273f5092b0315f
Ada Michocka <amichocka@wpp.pl>
2020-10-28 11:13:43 +0100

    initial commit

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ExampleTest {
+
+    @Test
+    void expample(){
+        fail();
+
+    }
+
+}

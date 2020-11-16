91172f25ca63a740ca0ff9956cae537a0af89068
Ada Michocka <amichocka@wpp.pl>
2020-11-16 15:44:27 +0100

    queue first implementation

--- src/main/java/pl/sdk/CreatureTurnQueue.java
+++ src/main/java/pl/sdk/CreatureTurnQueue.java
@@ -1,12 +1,28 @@
 package pl.sdk;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CreatureTurnQueue {
 
     private final Collection<Creature> creatures;
+    private final Queue<Creature> creatureQueue;
+    private Creature activeCreature;
 
     public CreatureTurnQueue(Collection<Creature> creatureList) {
         creatures = creatureList;
+        creatureQueue = new LinkedList<>();
+        creatureQueue.addAll(creatures);
+        next();
+    }
+
+    Creature getActiveCreature() {
+        return activeCreature;
+    }
+
+    void next() {
+        activeCreature = creatureQueue.poll();
+
     }
 }

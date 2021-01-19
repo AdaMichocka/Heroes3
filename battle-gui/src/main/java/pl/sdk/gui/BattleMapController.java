da3e7afcdbfd86eba35f5a2616353899a16c803a
Ada Michocka <amichocka@wp.pl>
2021-01-19 13:27:17 +0100

    first grid

--- battle-gui/src/main/java/pl/sdk/gui/BattleMapController.java
+++ battle-gui/src/main/java/pl/sdk/gui/BattleMapController.java
@@ -1,4 +1,23 @@
 package pl.sdk.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 public class BattleMapController {
+
+    @FXML
+    private GridPane gridMap;
+
+    @FXML
+    void initialize() {
+        for (int x = 0; x < 20; x++) {
+            for (int y = 0; y < 15; y++) {
+                Rectangle rec = new Rectangle(40, 40, Color.WHITE);
+                rec.setStroke(Color.BLACK);
+                gridMap.add(rec, x,y);
+            }
+        }
+    }
 }
